9540bdf35fc6be57b07faecad99e6894e98bcdf6
Kellie MacPhee <kmacphee@uw.edu>
2018-10-22 11:14:41 -0700

    updating step sizes

--- non_euclid_test.jl
+++ non_euclid_test.jl
@@ -16,7 +16,6 @@ using Random
 using LaTeXStrings
 using Printf
 using PyPlot
-using ColorBrewer
 
 include("func.jl");
 
@@ -24,9 +23,9 @@ include("func.jl");
 #   Parameters
 #-------------------------------------
 maxIter = 1000;
+r = 2;  # rank
 dims = [10, 100];
 dims_colors = ["#1f78b4", "#33a02c"];
-r = 2;  # rank
 
 Random.seed!(123);  # for reproducibility
 
@@ -49,16 +48,22 @@ for i in 1:length(dims)
     Xtrue = randn(d,r);
 
     # Generate initial point
-    radius = 1.0;
+    radius = 1.0;  # 2-norm measure of relative deviation between Xtrue and Xinit
     pert = randn(d,r);
-    X_init = Xtrue + radius * (norm(Xtrue, 2) / norm(pert, 2)) * pert;
+    Xinit = Xtrue + radius * (norm(Xtrue, 2) / norm(pert, 2)) * pert;
 
     # Specify step sizes
-    stepSizes = fill(0.01, maxIter) / d ;
+    τ = 10.0;
+    ρ = 0.0;  # linear models are convex
+    λ = 0.9 / (τ+ρ);
+
+    α = 0.01;  # any positive number
+    η = 1 / ( 1/λ + 1/α * sqrt(maxIter+1) );  # constant step size
+    stepSizes = fill(η, maxIter);
 
     # Run subgradient method
-    err_hist_subgrad = solve_cov_est_subgradient(X_init, Xtrue, stepSizes, maxIter)
-    err_hist_mirror = solve_cov_est_mirror(X_init, Xtrue, stepSizes, maxIter)
+    err_hist_subgrad = solve_cov_est_subgradient(Xinit, Xtrue, stepSizes, maxIter)
+    err_hist_mirror = solve_cov_est_mirror(Xinit, Xtrue, stepSizes, maxIter)
 
     # Add errors for this dimension to plot
     semilogy(err_hist_subgrad, linestyle="--", color=dims_colors[i], label=@sprintf("SGD, d=%i", d));
